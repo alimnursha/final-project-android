4b35df519f7bc7d6abbcd63cd21ef2b8e40451f2
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-17 23:46:14 +0600

    Add models(data class)

---
+++ app/src/main/java/com/example/weatherapp/core/model/Units.kt
@@ -0,0 +1,4 @@
+package com.example.weatherapp.core.model
+
+enum class Units {
+}
---
+++ app/src/main/java/com/example/weatherapp/core/model/Weather.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp.core.model
+
+data class Weather(
+    val latitude: Double,
+    val longitude: Double,
+    val hourlyWeather: HourlyWeather,
+    val currentWeather: CurrentWeather
+)
+
+data class HourlyWeather(val data: List<WeatherInfo>)
+
+data class CurrentWeather(val temperature: String)
+
+data class WeatherInfo(val temperature: String, val time: String)
