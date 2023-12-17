2b4c0794cbd622f13e7a1d1e307d58d17010314a
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-18 00:42:11 +0600

    Added units types enum class

--- app/src/main/java/com/example/weatherapp/core/model/Units.kt
+++ app/src/main/java/com/example/weatherapp/core/model/Units.kt
@@ -1,4 +1,6 @@
 package com.example.weatherapp.core.model
 
-enum class Units {
+enum class Units(val value: String, val tempLabel: String) {
+    METRIC("celsius","°C"),
+    IMPERIAL("fahrenheit","°F"),
 }
