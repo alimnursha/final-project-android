3db5176787ea2dfb07b5198f4e961df24c7c8509
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-18 00:41:37 +0600

    Added services to project

---
+++ app/src/main/java/com/example/weatherapp/data/OpenMeteoService.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp.data
+
+import com.example.weatherapp.core.model.Units
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenMeteoService {
+
+    @GET("/v1/forecast")
+    suspend fun getWeatherData(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("hourly") hourly:String = "temperature_2m",
+        @Query("temperature_unit") temperatureUnit: String = Units.METRIC.value,
+        @Query("forecast_days") forecastDays: Int = 1,
+        @Query("current_weather") includeCurrentWeather: Boolean = true
+    ): Response<WeatherResponse>
+
+}
---
+++ app/src/main/java/com/example/weatherapp/data/PollingService.kt
@@ -0,0 +1,19 @@
+package com.example.weatherapp.data
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+object PollingService {
+
+    private var _isPolling: AtomicBoolean = AtomicBoolean(true)
+
+    fun isPolling() = _isPolling.get()
+
+    fun startPolling() {
+        _isPolling.compareAndSet(false, true)
+    }
+
+    fun stopPolling() {
+        _isPolling.compareAndSet(true, false)
+    }
+
+}
