f68e7840b41be4db217feb31725aa3485ec38b6f
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-18 00:31:46 +0600

    Add Coordinates of cities

---
+++ app/src/main/java/com/example/weatherapp/data/Coordinates.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp.data
+
+data class Coordinate(val latitude: Double, val longitude: Double)
+
+val coordinates = listOf(
+    Coordinate(latitude = 43.238949, longitude = 76.889709),
+    Coordinate(latitude = 51.169392, longitude = 71.449074),
+    Coordinate(latitude = 53.619653, longitude = 10.079969),
+    Coordinate(latitude = 53.080917, longitude = 8.847533),
+    Coordinate(latitude = 52.378385, longitude = 9.794862),
+    Coordinate(latitude = 52.496385, longitude = 13.444041),
+    Coordinate(latitude = 53.866865, longitude = 10.739542),
+    Coordinate(latitude = 54.304540, longitude = 10.152741),
+    Coordinate(latitude = 54.797277, longitude = 9.491039),
+    Coordinate(latitude = 52.426412, longitude = 10.821392),
+    Coordinate(latitude = 53.542788, longitude = 8.613462),
+    Coordinate(latitude = 53.141598, longitude = 8.242565)
+)
