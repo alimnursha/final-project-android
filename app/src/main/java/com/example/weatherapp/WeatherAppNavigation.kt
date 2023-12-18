3f4a3021a876b070ccac936497da21e9bff8cb0e
Alimkhan Nursha <200101084@stu.sdu.edu.kz>
2023-12-18 15:59:41 +0600

    Implemented navigation

--- app/src/main/java/com/example/weatherapp/WeatherAppNavigation.kt
+++ app/src/main/java/com/example/weatherapp/WeatherAppNavigation.kt
@@ -1,2 +1,35 @@
 package com.example.weatherapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.weatherapp.ui.home.HomeScreen
+import com.example.weatherapp.ui.home.HomeScreenIntent
+import com.example.weatherapp.ui.home.HomeViewModel
+
+@Composable
+fun WeatherAppDestinationsConfig(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel,
+) {
+    NavHost(navController = navController, startDestination = Destinations.HOME.route) {
+        composable(Destinations.HOME.route) {
+            val state = homeViewModel
+                .state
+                .collectAsState()
+                .value
+
+            HomeScreen(
+                state = state,
+                onTryAgainClicked = { homeViewModel.processIntent(HomeScreenIntent.LoadWeatherData) },
+                onAddressReceived = {homeViewModel.processIntent(HomeScreenIntent.DisplayCityName(cityName = it))}
+            )
+        }
+    }
+}
+
+enum class Destinations(val route: String) {
+    HOME("home")
+}
